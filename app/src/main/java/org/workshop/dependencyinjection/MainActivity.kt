5b87f75023f560216a45169b84ed79685c2b2d28
Pornpat Santibuppakul <dottypurkt@gmail.com>
2021-01-13 11:39:05 +0700

    Implement UI

--- app/src/main/java/org/workshop/dependencyinjection/MainActivity.kt
+++ app/src/main/java/org/workshop/dependencyinjection/MainActivity.kt
@@ -2,8 +2,16 @@ package org.workshop.dependencyinjection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.workshop.dependencyinjection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // ViewBinding
+    private lateinit var binding: ActivityMainBinding
+
+    // ViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/org/workshop/dependencyinjection/model/Monster.kt
+++ app/src/main/java/org/workshop/dependencyinjection/model/Monster.kt
@@ -13,10 +13,13 @@ class Monster(
     val totalAtk get() = baseAtk + weapon.atk
     val totalDef get() = baseDef + armor.def
 
+    enum class MonsterState { NORMAL, GUARD }
+    var state: MonsterState = MonsterState.NORMAL
+
     fun attack(target: Monster) {
         val elementMultiplier = getElementMultiplier(target)
         val damage = (totalAtk - (0.25 * totalDef)) * elementMultiplier
-        target.hp -= damage
+        target.hp -= (damage * if (state == MonsterState.GUARD) 0.5 else 1.0)
         Log.d("DependencyInjection", "${target.name} takes $damage damage by $name!")
         Log.d("DependencyInjection", "${target.name}'s current hp is ${target.hp}")
     }
